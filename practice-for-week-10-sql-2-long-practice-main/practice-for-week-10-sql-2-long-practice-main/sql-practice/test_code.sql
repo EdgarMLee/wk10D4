174d797c94f275216e910e6da9ad7008ec8b885c
edgar <edgarlee135@gmail.com>
2022-06-16 13:13:13 -0700

    lzgit - 2022-06-16T20:13:06.692Z

---
+++ practice-for-week-10-sql-2-long-practice-main/practice-for-week-10-sql-2-long-practice-main/sql-practice/test_code.sql
@@ -0,0 +1,21 @@
+-- PHASE 1
+CREATE TABLE Cats (
+ name varchar(50),
+ birth_year integer
+ );
+
+CREATE TABLE Owners (
+ first_name varchar(15),
+ last_name varchar(15)
+ );
+
+CREATE TABLE Toys (
+ name varchar(15)
+);
+
+CREATE TABLE cat_owner (
+  cat_id INTEGER,
+  owner_id INTEGER,
+  FOREIGN KEY (cat_id) REFERENCES Cats(id)
+  FOREIGN KEY (owner_id) REFERENCES Owners(id)
+)
