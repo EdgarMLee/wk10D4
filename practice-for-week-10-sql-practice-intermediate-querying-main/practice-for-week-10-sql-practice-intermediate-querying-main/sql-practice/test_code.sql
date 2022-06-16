47b1ddc280f050461f7a411676cd67614fdf1c88
edgar <edgarlee135@gmail.com>
2022-06-16 10:19:18 -0700

    lzgit - 2022-06-16T17:19:12.228Z

--- practice-for-week-10-sql-practice-intermediate-querying-main/practice-for-week-10-sql-practice-intermediate-querying-main/sql-practice/test_code.sql
+++ practice-for-week-10-sql-practice-intermediate-querying-main/practice-for-week-10-sql-practice-intermediate-querying-main/sql-practice/test_code.sql
@@ -11,4 +11,10 @@ SELECT * FROM albums WHERE band_id IN (1,3,4);
 -- Q4
 SELECT * FROM albums WHERE title ILIKE "the%";
 
+-- Q5
+SELECT * FROM albums ORDER BY num_sold LIMIT 2;
 
+-- Q6
+SELECT * FROM albums
+ORDER BY num_sold DESC
+LIMIT 2 OFFTSET 2;
